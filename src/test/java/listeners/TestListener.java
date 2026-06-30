f30b42289764548430810848382fa33a829b9746
purushothr15-star <purushoth.r.15@gmail.com>
2026-06-30 15:42:46 +0530

    added comment

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -63,6 +63,7 @@ public class TestListener  implements ITestListener {
 
 
     //have you used override concept any where in you project?
+    //in ITestListeners and IRetryAlalyzer
     /*ExtentReports extent = ExtentManager.getInstance();
 
     @Override
